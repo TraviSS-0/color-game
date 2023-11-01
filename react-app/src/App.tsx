dd74c3c274389f6a02865fa7efd8d7428b3cc81f
TraviSS-0 <146763084+TraviSS-0@users.noreply.github.com>
2023-11-01 18:44:10 +0100

    Finished

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import "./style.css";
+const availableColors = ["Red", "Blue", "Yellow", "Green"];
 
 export default function App() {
-  const availableColors = [
-    "Your Color is: Red",
-    "Your Color is: Blue",
-    "Your Color is: Yellow",
-    "Your Color is: Green",
-    "You Lost",
-  ];
   const [userScore, setUserScore] = useState(0);
   const [userHighestScore, setUserHighestScore] = useState(0);
-  const [restartButton, setRestartButton] = useState(false);
+  const [isRestartButtonAvailable, setIsRestartButtonAvailable] =
+    useState(false);
   const [randomNumber, setRandomNumber] = useState(
     Math.floor(Math.random() * 4)
   );
@@ -23,10 +18,10 @@ export default function App() {
   };
 
   // Handle button click
-  const handleButtonClick = (buttonId: string) => {
-    let number = parseInt(buttonId, 10);
+  const handleButtonClick = (buttonId: number) => {
+    let number = buttonId;
 
-    if (number === randomNumber && !restartButton) {
+    if (number === randomNumber && !isRestartButtonAvailable) {
       setUserScore(userScore + 1);
       handleRandomize();
       if (userScore >= userHighestScore) {
@@ -34,43 +29,43 @@ export default function App() {
       }
     } else {
       setUserScore(0);
-      setRestartButton(true);
+      setIsRestartButtonAvailable(true);
     }
   };
 
   return (
     <div className="mainFlex">
-      <h1 id="YourColor">{availableColors[randomNumber]}</h1>
+      <h1 id="YourColor">Your Color is: {availableColors[randomNumber]}</h1>
       <div className="buttons">
         <button
           type="button"
           id="0"
-          onClick={() => handleButtonClick("0")}
+          onClick={() => handleButtonClick(0)}
         ></button>
         <button
           type="button"
           id="1"
-          onClick={() => handleButtonClick("1")}
+          onClick={() => handleButtonClick(1)}
         ></button>
         <button
           type="button"
           id="2"
-          onClick={() => handleButtonClick("2")}
+          onClick={() => handleButtonClick(2)}
         ></button>
         <button
           type="button"
           id="3"
-          onClick={() => handleButtonClick("3")}
+          onClick={() => handleButtonClick(3)}
         ></button>
       </div>
       <h2 id="score">Score: {userScore}</h2>
       <h2 id="highestScore">Your highest score: {userHighestScore}</h2>
-      {restartButton && (
+      {isRestartButtonAvailable && (
         <button
           type="button"
           id="restart"
           onClick={() => {
-            setRestartButton(false);
+            setIsRestartButtonAvailable(false);
             setUserScore(0);
             handleRandomize();
           }}
